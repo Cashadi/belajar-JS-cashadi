69d3e7e4524bca392dea6ec96831f6a63022a71e
Cashadi <ichascashadi@gmail.com>
2023-06-20 23:36:24 +0700

    sudah malam

--- const-blockScope.js
+++ const-blockScope.js
@@ -1,8 +1,23 @@
-const job = 'Data Science';
+// const job = 'Data Science';
 
-{
-  const job = 'Data Analysis';
-}
+// {
+//   const job = 'Data Analysis';
+// }
 
-console.log(job);
+// console.log(job);
 // console log yang akan kepanggil adalah yang diluar tanda kurawal {}
+
+// const-blockScope.js (biar nggk lama nulis)
+
+// merubah array dalam bentuk const
+
+const ngabs = ['vario', 'beat', 'aerox', 'vesmet'];
+
+// cara ganti
+ngabs[0] = 'scoopy';
+
+// menambahkan array
+ngabs.push('vario');
+
+// kita console
+console.log(ngabs);
