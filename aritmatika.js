c8fc0cebac59ab9879e187d5ee148532edd158ed
Cashadi <ichascashadi@gmail.com>
2023-06-22 15:07:26 +0700

    operator JS

---
+++ aritmatika.js
@@ -0,0 +1,5 @@
+// operator penjumlahan
+let a = 10;
+let x = (100 + 50) * a;
+
+console.log(x);
---
+++ operator.js
@@ -0,0 +1,8 @@
+// kasih nilai 10 di X
+let x = 10;
+// kasih nilai 7 di Y
+let y = 7;
+// penjumlahan X and Y
+z = x + y;
+// console.log(z);
+console.log(z);
